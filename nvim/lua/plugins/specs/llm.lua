d561b100d41edb15c38778de5bfe59d89622b594
Omar Contreras <oc@hellofresh>
2025-04-18 20:53:45 +0200

    Set Claude 3.7 as default

--- nvim/lua/plugins/specs/llm.lua
+++ nvim/lua/plugins/specs/llm.lua
@@ -5,16 +5,13 @@ return {
         event = "VeryLazy",
         version = false, -- Never set this value to "*"! Never!
         opts = {
-            -- add any opts here
-            -- for example
-            provider = "openai",
-            openai = {
-                endpoint = "https://api.openai.com/v1",
-                model = "gpt-4o",   -- your desired model (or use gpt-4o, etc.)
-                timeout = 30000,    -- Timeout in milliseconds, increase this for reasoning models
+            claude = {
+                endpoint = "https://api.anthropic.com",
+                model = "claude-3-7-sonnet-20250219",
+                timeout = 30000, -- Timeout in milliseconds
                 temperature = 0,
-                max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-                --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+                max_tokens = 4096,
+                disable_tools = true, -- disable tools!
             },
         },
         -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
@@ -26,12 +23,12 @@ return {
             "nvim-lua/plenary.nvim",
             "MunifTanjim/nui.nvim",
             --- The below dependencies are optional,
-            "echasnovski/mini.pick", -- for file_selector provider mini.pick
+            "echasnovski/mini.pick",         -- for file_selector provider mini.pick
             "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-            "hrsh7th/nvim-cmp",      -- autocompletion for avante commands and mentions
-            "ibhagwan/fzf-lua",      -- for file_selector provider fzf
-            "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-            "zbirenbaum/copilot.lua", -- for providers='copilot'
+            "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
+            "ibhagwan/fzf-lua",              -- for file_selector provider fzf
+            "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
+            "zbirenbaum/copilot.lua",        -- for providers='copilot'
             {
                 -- support for image pasting
                 "HakonHarnes/img-clip.nvim",
