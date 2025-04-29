7ca4cd387b20cd66da9f92f9d9aff97094a4e213
Omar Contreras <oc@hellofresh.com>
2025-04-29 15:27:28 +0200

    Enable more plugins on Catppuccin

--- nvim/lua/plugins/specs/ui.lua
+++ nvim/lua/plugins/specs/ui.lua
@@ -43,7 +43,11 @@ return {
         "nvim-lualine/lualine.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons", lazy = true },
         config = function()
-            require("lualine").setup()
+            require("lualine").setup {
+                options = {
+                    theme = "catppuccin",
+                },
+            }
         end
     },
     {
