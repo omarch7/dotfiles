53096c4404f641a374cbb7334271ab4f5bcd02e9
Omar Contreras <oc@hellofresh>
2025-05-29 13:49:10 +0200

    Style DAP with catppuccin

--- nvim/lua/plugins/specs/dap.lua
+++ nvim/lua/plugins/specs/dap.lua
@@ -1,6 +1,23 @@
 return {
     {
-        "mfussenegger/nvim-dap"
+        "mfussenegger/nvim-dap",
+        config = function()
+            require("dap").set_log_level("TRACE")
+            require("dap").defaults.fallback.terminal_win_cmd = "50vsplit new"
+
+            local catppuccin = require("catppuccin.palettes").get_palette()
+
+            -- Set up highlight groups
+            vim.api.nvim_set_hl(0, 'DapBreakpoint', { fg = catppuccin.red })
+            vim.api.nvim_set_hl(0, 'DapLogPoint', { fg = catppuccin.blue })
+            vim.api.nvim_set_hl(0, 'DapStopped', { fg = catppuccin.green })
+
+            -- Define signs
+            vim.fn.sign_define('DapBreakpoint', { text = '●', texthl = 'DapBreakpoint' })
+            vim.fn.sign_define('DapBreakpointCondition', { text = '◆', texthl = 'DapBreakpoint' })
+            vim.fn.sign_define('DapLogPoint', { text = '◉', texthl = 'DapLogPoint' })
+            vim.fn.sign_define('DapStopped', { text = '▶', texthl = 'DapStopped' })
+        end,
     },
     {
         "mfussenegger/nvim-dap-python",
