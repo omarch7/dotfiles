05ca10f2da5f4fac885e451c17b852fe4ddb2df4
Omar Contreras <oc@hellofresh.com>
2025-06-05 17:43:38 +0200

    Use zbirenbaum copilot for custom keymaps

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -26,3 +26,17 @@ vim.api.nvim_create_autocmd("BufEnter", {
         end
     end,
 })
+
+vim.api.nvim_create_autocmd("User", {
+    pattern = "BlinkCmpMenuOpen",
+    callback = function()
+        vim.b.copilot_suggestion_hidden = true
+    end,
+})
+
+vim.api.nvim_create_autocmd("User", {
+    pattern = "BlinkCmpMenuClose",
+    callback = function()
+        vim.b.copilot_suggestion_hidden = false
+    end,
+})
