e7fe16550954475f67bde5c369482475acfa89b7
Omar Contreras <oc@hellofresh.com>
2025-11-11 18:07:42 +0100

    Remove unused params

--- nvim/lua/plugins/specs/ufo.lua
+++ nvim/lua/plugins/specs/ufo.lua
@@ -14,7 +14,7 @@ return {
             vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
 
             require('ufo').setup({
-                provider_selector = function(bufnr, filetype, buftype)
+                provider_selector = function()
                     return { 'lsp', 'indent' }
                 end
             })
