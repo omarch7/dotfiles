99edb4ee438433747427105f123544d331e74f58
Omar Contreras <omarch7@gmail.com>
2026-03-01 19:57:58 +0000

    Default vim-test to uv for Python

--- nvim/lua/plugins/specs/vim-test.lua
+++ nvim/lua/plugins/specs/vim-test.lua
@@ -1,3 +1,8 @@
 return {
-    { "vim-test/vim-test" },
+    {
+        "vim-test/vim-test",
+        init = function()
+            vim.g["test#python#pytest#executable"] = "uv run pytest"
+        end,
+    },
 }
