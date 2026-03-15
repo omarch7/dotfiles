750ca91f1cd175d84cc1a91735cdedfa508ecf77
Omar Contreras <omarch7@gmail.com>
2026-03-15 22:15:08 +0100

    Reconfigure Catppuccin theme

--- nvim/lua/plugins/specs/catppuccin.lua
+++ nvim/lua/plugins/specs/catppuccin.lua
@@ -5,9 +5,30 @@ return {
         priority = 1000,
         config = function()
             require("catppuccin").setup {
-                cmp = true,
-                treesitter = true,
-                diffview = true,
+                flavour = "auto",
+                background = {
+                    light = "latte",
+                    dark = "mocha",
+                },
+                integrations = {
+                    cmp = true,
+                    treesitter = true,
+                    diffview = true,
+                    gitsigns = true,
+                    notify = true,
+                    dashboard = true,
+                    fzf = true,
+                    lualine = true,
+                    dap_ui = true,
+                    dap = true,
+                    render_markdown = true,
+                    snacks = {
+                        enabled = true,
+                    },
+                    telescope = true,
+                    which_key = true,
+
+                },
             }
         end,
     },
--- nvim/lua/plugins/specs/lualine.lua
+++ nvim/lua/plugins/specs/lualine.lua
@@ -4,7 +4,7 @@ return {
         dependencies = { "nvim-tree/nvim-web-devicons", lazy = true },
         opts = {
             options = {
-                theme = "catppuccin",
+                theme = "catppuccin-nvim",
                 component_separators = "",
                 section_separators = { left = "", right = "" },
             },
